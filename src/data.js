bcd6dfec16cdeb7a9a4b20dfeec54a56f64cc6b2
Vivek Kumar <104918205+VivekKumar-8287@users.noreply.github.com>
2022-12-29 16:22:24 +0000

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    name: 'Mahesh',
+    age: 29,
+    image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg'
+},
+   {
+    id: 2,
+    name: 'Jitendra',
+    age: 21,
+    image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg'
+},
+   {
+    id: 3,
+    name: 'Raman',
+    age: 25,
+    image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg'
+},
+   {
+    id: 4,
+    name: 'Prateek',
+    age: 20,
+    image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg'
+}
+];
